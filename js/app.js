ae5d8d42fdd7b3b334a71ce3bf089363c4e3d4e5
Hozz <medo3578@gmail.com>
2016-07-14 17:35:26 +0200

    v0.3

--- js/app.js
+++ js/app.js
@@ -1,39 +1,40 @@
-'use strict';
-$(".start").click(function(){
+$(".start").click(function () {
     // store the input as an array
-    var star = $('.input')[0].value;
-    star = star.replace(/\s/ig, " ");
+    var star = $(".input")[0].value;
+    star = star.replace(/\s/g, " ");
+    console.log(star);
     star = star.split(" ");
 
     // Words per minute
-    var wordsPerMinute = $('.speed')[0].value;;
-    var speed = 60000/wordsPerMinute;
+    var wordsPerMinute = $(".speed")[0].value;
+    var speed = 60000 / wordsPerMinute;
 
     // let there be words
-    var intervalHandle = setInterval(function(){
-        $('.reading-area').html(star.shift());
-        if(star.length === 0){
+    var intervalHandle = setInterval(function () {
+        $(".reading-area").html(star.shift());
+        if (star.length === 0) {
             // clear the interval when the array is empty
             clearInterval(intervalHandle);
             $(".reset").css("display", "inline-block");
         }
     }, speed);
-    $(".staging-area").css("display","none");
+    $(".staging-area").css("display", "none");
 });
 
-$('.reset').click(function(){
+// reset everything to staging
+$(".reset").click(function () {
     $(".staging-area").css("display", "flex");
     $(".reset").css("display", "none");
     $(".reading-area").html("");
 });
 
+// template setting
+$(".ar").click(function () {
+    var str = "البرنامج هو أشياء كثيرة. انه نص كتبه مبرمج؛ وهو القوة الموجهة التي تجعل الحاسوب يقوم بما يقوم به؛ وهو البيانات التي في ذاكرة الحاسوب، ورغم ذاك هو الذي يتحكم بما يحدث في نفس الذاكرة التي يعيش بها. جافاسكربت مقنعة .. مارين هافربيكي (اللي هيعلق على الترجمة يستعد لغضبي =D :)";
+    $(".input")[0].value = str;
+});
 
-/*
-
-when the array is empty
-we should display the reset button
-
- */
-
-
-
+$(".en").click(function () {
+    var str = "A program is many things. It is a piece of text typed by a programmer; it is the directing force that makes the computer do what it does; it is data in the computer’s memory, yet it controls the actions performed on this same memory. Eloquent Javascript .. Marijn Haverbeke";
+    $(".input")[0].value = str;
+});
